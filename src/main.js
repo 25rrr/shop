23819a61bd4e6cbc3fb837f58de60ed26c36049a
25rrr <3031856092@qq.com>
2022-07-18 09:21:56 +0800

    登录完成

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import './plugins/element.js'
-
+import './assets/css/global.css'
+import axios from 'axios'
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://liufusong.top:8899/api/private/v1/'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
---
+++ src/views/Welcome/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <el-container>
+      <el-header
+        >杭州黑马后台管理系统
+        <el-button type="info" @click="logout">退出</el-button></el-header
+      >
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  methods: {
+    // 退出登录
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
